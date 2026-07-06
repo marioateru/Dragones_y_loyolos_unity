b1860816157e5e9cfd794a663662a49d445c29b0
MarioAlfonsoDoesStuff <marioateru@gmail.com>
2026-07-06 12:03:28 +0200

    cambios en script de prueba

--- dragones_y_loyolos/Assets/Scripts/PruebaBaseDatos.cs
+++ dragones_y_loyolos/Assets/Scripts/PruebaBaseDatos.cs
@@ -15,31 +15,26 @@ public class PruebaBaseDatos : MonoBehaviour
 {
     void Start()
     {
-        // 1. Conectamos a la base de datos que acabas de meter
         string dbPath = string.Format("{0}/{1}", Application.streamingAssetsPath, "dragones_y_loyolos.db");
         
-        // Copiamos la DB al almacenamiento persistente si es necesario (como vimos en tu DataService)
         string filepath = string.Format("{0}/{1}", Application.persistentDataPath, "dragones_y_loyolos.db");
         if (!System.IO.File.Exists(filepath)) {
             System.IO.File.Copy(dbPath, filepath);
         }
         Debug.Log(filepath);
 
-        // Abrimos conexión
         var connection = new SQLiteConnection(filepath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
 
-        // 2. Extraemos el jugador que metiste en el script (id_entidades = 1)
         var miJugador = connection.Table<DatosJugadorSQL>().Where(j => j.id_entidades == 1).FirstOrDefault();
 
-        // 3. Comprobamos si ha funcionado
         if (miJugador != null)
         {
-            Debug.Log("¡ÉXITO! Base de datos conectada.");
+            Debug.Log("Base de datos conectada.");
             Debug.Log($"He cargado al jugador: {miJugador.id_jugadores}, Raza: {miJugador.raza}, Nivel: {miJugador.nivel}");
         }
         else
         {
-            Debug.LogError("No he encontrado al jugador. Algo ha fallado en la lectura.");
+            Debug.LogError("No he encontrado al jugador. Conexión fallida.");
         }
     }
 }
